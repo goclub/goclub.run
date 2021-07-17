bb314d4b805183f499b14479abf09ea8c3435542
nimoc <nimocc@126.com>
2021-07-17 15:09:57 +0800

    pushState

--- main.js
+++ main.js
@@ -12,16 +12,15 @@ components[SpecEnum.name] = SpecEnum
 components[SpecModel.name] = SpecModel
 components[GenQrcode.name] = GenQrcode
 components[GenUUID.name] = GenUUID
-const TAG_KEY = "tag_key_v1"
 const data = function () {
     return {
-        activeTool: qs.parse(location.search).kind || localStorage.getItem(TAG_KEY) || "model"
+        activeTool: qs.parse(location.search).kind || "model"
     }
 }
 const methods = {
     changeTag: function (value, e) {
         const vm = this
-        localStorage.setItem(TAG_KEY, vm.activeTool)
+        history.pushState({}, "", `${location.pathname}?kind=${vm.activeTool}`);
     }
 }
 var app = new Vue({
