e0070a827afd99f17de7512ba924e458fb814b6e
nimoc <nimocc@126.com>
2022-01-30 15:06:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -21,11 +21,12 @@ const data = function () {
     return {
         activeTool: qs.parse(location.search).k || "repo",
         repos: [
-            ["sql", "基于 sqlx 实现更易用的sql, 接口优雅的同时媲美 sqlx 的性能"],
             ["error", "错误处理指南和错误码"],
+            ["validator", "类型安全的结构体数据验证器"],
             ["http", "Go 官方 net/http 标准库和 mux 的扩展, 对错误处理友好"],
             ["session", "安全易用的 session golang 库"],
-            ["validator", "类型安全的结构体数据验证器"],
+            ["sql", "基于 sqlx 实现更易用的sql, 接口优雅的同时媲美 sqlx 的性能"],
+            ["sync", "go 并发编程教程和工具"],
             ["json", "go 宽容的JSON处理, Marshal 支持 nil => [] , Unmarshal 支持 string => int or float"],
             ["time", "time 的一些便捷扩展"],
         ]
