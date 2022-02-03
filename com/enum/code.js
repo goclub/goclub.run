1ec9a7ec9c2f4ea00d4a9a3cafc6d98c9f272273
nimoc <nimocc@126.com>
2022-02-03 15:32:43 +0800

    Update code.js

--- com/enum/code.js
+++ com/enum/code.js
@@ -1,7 +1,7 @@
 export default `package m
 // You can change package name
 
-// <#= v.name #> Generate by https://goclub.run
+// <#= v.name #> Generate by https://goclub.run/?k=enum
 // ---------------------- DO NOT EDIT (Begin) ----------------------
 // Source enums:
 // <#- JSON.stringify(v) #>
